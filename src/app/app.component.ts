40268a6bf62798a1070ff3bc89732ee66cad8ce4
Alessio Ilardi <alessio.ilardi@gmail.com>
2024-07-25 18:00:43 +0200

    feat: Add UserInterface and data to initialize users in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,59 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { UserInterface } from './user.interface';
+import { data } from './data';
+import { CommonModule } from '@angular/common';
+import {
+  CdkDrag,
+  CdkDragDrop,
+  CdkDropList,
+  CdkDropListGroup,
+  moveItemInArray,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    CommonModule,
+    CdkDropListGroup,
+    CdkDropList,
+    CdkDrag,
+  ],
 })
 export class AppComponent {
-  title = 'drag-drop-material-angular-project';
+  users: UserInterface[] = data;
+  drop(event: CdkDragDrop<UserInterface[]>): void {
+    moveItemInArray(
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
+  addUser() {
+    this.users.push({
+      name: '',
+      id: Math.max(...this.users.map((user) => user.id)) + 1,
+    });
+  }
+  removeUser({ id }: UserInterface) {
+    this.users = this.users.filter((user) => user.id !== id);
+  }
+  resizeElement(event: MouseEvent, element: HTMLElement): void {
+    const initialWidth = element.offsetWidth;
+    const initialX = event.clientX;
+    const mouseMove = (moveEvent: MouseEvent): void => {
+      element.style.width = `${initialWidth + moveEvent.clientX - initialX}px`;
+    };
+    const mouseUp = (): void => {
+      document.removeEventListener('mousemove', mouseMove);
+      document.removeEventListener('mouseup', mouseUp);
+    };
+    document.addEventListener('mousemove', mouseMove);
+    document.addEventListener('mouseup', mouseUp);
+  }
 }
---
+++ src/app/data.ts
@@ -0,0 +1,14 @@
+export const data = [
+  {
+    id: 1,
+    name: 'Kathleen',
+  },
+  {
+    id: 2,
+    name: 'Arlina',
+  },
+  {
+    id: 3,
+    name: 'Lorilyn',
+  },
+];
